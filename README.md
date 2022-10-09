0a26adf5e1500ad722e38f3f49126349f0b1c437
DavidCrompton1192@gmail.com <DavidCrompton1192@gmail.com>
2022-10-09 19:46:16 -0400

    Basic Deno Discordeno Setup
    In future we'll want to organize text processing and commands into
    separate files/modules.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 The point of this Discord Bot is many, many reasons.
 
+The bot may be run simply with:
+
+```bash
+DISCORD_TOKEN="TOKEN HERE" deno run -A main.js
+```
+
 Created by Ethan Foshee on October 7th, 2022
 Collabs are (as of October 7th, 2022) David C and Ben R
---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Denocord Discord Bot";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = with pkgs; [
+          deno
+        ];
+      };
+    });
+}
