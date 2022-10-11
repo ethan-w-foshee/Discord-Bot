8c40123edbc5069f9592ec4ae21a0a5d2c63ca74
ethan.w.foshee <skyhailer2@gmail.com>
2022-10-11 18:25:27 -0500

    Created file structure, moved roll command to roll.js, and added some IDs

---
+++ commands/roll.js
@@ -0,0 +1,65 @@
+import * as discordeno from "https://deno.land/x/discordeno@16.0.1/mod.ts";
+
+export default function rolling(bot, msg) {
+
+    let cont = msg.content;
+
+    /* Parses and resends message after correcting for
+     * embedded rolls and variable outcome statements.
+     * Example Statement:
+     * "#I roll a {1d20}" -> "I roll a 13"
+     *               ^-- Roll subtitution
+     * "#{1d20>=20?You ran away!:You tripped}"
+     */
+    const rolls = cont.matchAll(/\{([^}]+)}/g);
+    /* Parses message and returns contents of brackets in
+     * the following form:
+     * "{This} message {is}"
+     * [ [ "{This}", "This" ], [ "{is}", "is"] ]
+     */
+    let replacements = [];
+    for (const roll_ of rolls) {
+        let sum = 0;
+        const roll = roll_[1];
+        const comparisons = /(>|<|=)(=)?/;
+        const comparator = roll.search(comparisons);
+        const dice = comparator == -1 ?
+            roll : roll.substring(0, comparator);
+        const dies = dice.split(' ');
+        for (const die of dies) {
+            const nums = die.split('d')
+            if (nums.length == 1) {
+                sum += parseInt(nums[0], 10);
+            } else if (nums.length == 2) {
+                const sides = parseInt(nums[1], 10);
+                const times = parseInt(nums[0], 10);
+                for (let i = 0; i < times; i++) {
+                    sum += Math.floor(Math.random() * sides) + 1;
+                }
+            }
+        }
+        replacements.push(sum);
+        /* if a compararison is made */
+        if (comparator != -1) {
+            console.log("TODO: Verify Comparison Format")
+            const symbol = roll.match(comparisons);
+            switch (symbol) {
+                case '>':
+                    break;
+                case '>=':
+                    break;
+                case '<':
+                    break;
+                case '<=':
+                    break;
+                case '=':
+                case '==':
+                    break;
+            }
+        }
+    }
+    discordeno.sendMessage(bot, msg.channelId, {
+        content: replacements.toString()
+    });
+
+}
--- main.js
+++ main.js
@@ -1,75 +1,23 @@
 import * as discordeno from "https://deno.land/x/discordeno@16.0.1/mod.ts";
+import { UserFlags } from "https://deno.land/x/discordeno@16.0.1/mod.ts";
+import rolling from "./commands/roll.js"
+import { hAndh , botId} from "./ids/ids.js";
 
 const bot = discordeno.createBot({
-    intents: discordeno.Intents.Guilds | discordeno.Intents.GuildMessages | discordeno.Intents.MessageContent,
-    token: Deno.env.get("DISCORD_TOKEN"),
-    events: {
-	ready() {
-	    console.log("Successfully connected to gateway");
-	},
-	messageCreate(bot, msg) {
-	    let cont = msg.content;
-	    if (cont.startsWith('#')) {
-		/* Parses and resends message after correcting for
-		 * embedded rolls and variable outcome statements.
-		 * Example Statement:
-		 * "#I roll a {1d20}" -> "I roll a 13"
-		 *               ^-- Roll subtitution
-		 * "#{1d20>=20?You ran away!:You tripped}"
-		 */
-		const rolls = cont.matchAll(/\{([^}]+)}/g);
-		/* Parses message and returns contents of brackets in
-		 * the following form:
-		 * "{This} message {is}"
-		 * [ [ "{This}", "This" ], [ "{is}", "is"] ]
-		 */
-		let replacements = [];
-		for (const roll_ of rolls) {
-		    let sum = 0;
-		    const roll = roll_[1];
-		    const comparisons = /(>|<|=)(=)?/;
-		    const comparator = roll.search(comparisons);
-		    const dice = comparator==-1?
-			  roll:roll.substring(0,comparator);
-		    const dies = dice.split(' ');
-		    for (const die of dies) {
-			const nums = die.split('d')
-			if (nums.length==1) {
-			    sum += parseInt(nums[0],10);
-			}else if (nums.length==2) {
-			    const sides = parseInt(nums[1],10);
-			    const times = parseInt(nums[0],10);
-			    for (let i=0; i<times; i++) {
-				sum += Math.floor(Math.random()*sides)+1;
-			    }
+	intents: discordeno.Intents.Guilds | discordeno.Intents.GuildMessages | discordeno.Intents.MessageContent,
+	token: Deno.env.get("DISCORD_TOKEN"),
+	events: {
+		ready() {
+			console.log("Successfully connected to gateway");
+		},
+		messageCreate(bot, msg) {
+			if (msg.authorId == botId || msg == " ") {
+				console.log("This is my own message.")
+			} else {
+				rolling(bot, msg)
 			}
-		    }
-		    replacements.push(sum);
-		    /* if a compararison is made */
-		    if (comparator != -1) {
-			console.log("TODO: Verify Comparison Format")
-			const symbol = roll.match(comparisons);
-			switch (symbol) {
-			case '>':
-			    break;
-			case '>=':
-			    break;
-			case '<':
-			    break;
-			case '<=':
-			    break;
-			case '=':
-			case '==':
-			    break;			    
-			}
-		    }
 		}
-		discordeno.sendMessage(bot, msg.channelId, {
-		    content: replacements.toString()
-		});
-	    }
 	}
-    }
 });
 
 await discordeno.startBot(bot);
