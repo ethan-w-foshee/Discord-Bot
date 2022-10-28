19b546b76b0307fc8e7bc98739bcb13273818c04
DavidCrompton1192@gmail.com <DavidCrompton1192@gmail.com>
2022-10-28 18:33:48 -0400

    Names for commands must be lowercase
    TODO: impl. auto tolower

--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -3,50 +3,51 @@ import { addBotCommand } from "../../lib/commands.js";
 import { editOriginalInteractionResponse,
 	 sendInteractionResponse,
 	 InteractionResponseTypes,
-     kickMember } from "../../../deps.js";
+	 ApplicationCommandOptionTypes,
+	 kickMember } from "../../../deps.js";
 
 
 function kickSlash(bot, interaction) {
     sendInteractionResponse(bot, interaction.id, interaction.token, {
         type: InteractionResponseTypes.DeferredChannelMessageWithSource
-        });
+    });
     
     const options = interaction.data.options
     guild = interaction.guildId
     user = options.filter(option => option.User == "User")[0].value
     reason = options.filter(option => option.Reason == "Reason")[0].value
     kickMember(
-            {
+        {
             bot: bot,
             guildId: guild,
             userId: user,
             reason: reason
-            }
-        );
+        }
+    );
     editOriginalInteractionResponse(bot,
-        interaction.token,
-            {
-                content:`User ${user} kicked for ${reason}.`
-            }
-        );
+				    interaction.token,
+				    {
+					content:`User ${user} kicked for ${reason}.`
+				    }
+				   );
 }
 
 addBotCommand(bot, {
     type: "slash",
-    default_member_permissions: 0x2,
-    name: "kick",
+    // default_member_permissions: "0x2",
+    name: "sKick",
     description: "Get rid of someone, anyone (within reason), temporarily.",
     options: [{
-        name: "User",
+        name: "user",
         description: "The user to remove",
         type: ApplicationCommandOptionTypes.User,
         required: true
-        },{
-        name: "Reason",
+    },{
+        name: "reason",
         description: "Why they are being removed",
         type: ApplicationCommandOptionTypes.String,
         required: true,
-        }],
+    }],
     actions: [
 	kickSlash
     ]
