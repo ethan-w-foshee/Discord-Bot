c7c707b6c70ca7f7d291774ff7676f2a31e35612
DavidCrompton1192@gmail.com <DavidCrompton1192@gmail.com>
2022-10-29 11:53:01 -0400

    Create template log command, based on ping

---
+++ src/commands/log.js
@@ -0,0 +1,28 @@
+import { bot } from "../../bot.js";
+import { addBotCommand } from "../lib/commands.js";
+import {
+  editOriginalInteractionResponse,
+  InteractionResponseTypes,
+  sendInteractionResponse,
+} from "../../deps.js";
+
+function pong(bot, interaction) {
+  sendInteractionResponse(bot, interaction.id, interaction.token, {
+    type: InteractionResponseTypes.DeferredChannelMessageWithSource,
+  });
+  const now = Date.now();
+  const interactionSent = Number(interaction.id / 4194304n + 1420070400000n);
+
+  editOriginalInteractionResponse(bot, interaction.token, {
+    content: `Pong! ${now - interactionSent} ms`,
+  });
+}
+
+addBotCommand(bot, {
+  type: "slash",
+  name: "log",
+  description: "Poke the bot, just a bit",
+  actions: [
+    pong,
+  ],
+});
