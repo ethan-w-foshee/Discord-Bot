0323a454293b279690c579b8019e49baf7a5398a
DavidCrompton1192@gmail.com <DavidCrompton1192@gmail.com>
2022-10-29 11:51:51 -0400

    Update commands after ready
    This ensures the bot will be able to respond to Interaction PINGs,
    letting commands load faster

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 import { createBot, Intents } from "./deps.js";
-import { addBotCommand, enableCommandsPlugin } from "./src/lib/commands.js";
+import { addBotCommand, enableCommandsPlugin, updateBotCommands } from "./src/lib/commands.js";
 import { logger } from "./logger.js";
 
 export const bot = createBot({
@@ -17,5 +17,6 @@ addBotCommand(bot, {
     event: "ready",
     actions: [function (bot) {
 	bot.logger.info("Connected to gateway! Bot online!");
+	updateBotCommands(bot);
     }],
 });
--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // This file responsible for coordinating all other files and actually starting the bot
 import { startBot, walkSync } from "./deps.js";
 import { bot } from "./bot.js"; // bot instance
-import { updateBotCommands } from "./src/lib/commands.js";
 
 // Import all commands in commands directory
 for (const file of walkSync("./src/commands")) {
@@ -9,8 +8,6 @@ for (const file of walkSync("./src/commands")) {
 	if (file.name.endsWith(".js"))
 	    import("./"+file.path);
 }
-
-updateBotCommands(bot);
 
 // Start the bot
 await startBot(bot);
