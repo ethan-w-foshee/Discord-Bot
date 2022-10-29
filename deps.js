b32eabf69942f317e4ca3f53cadda3f95ee17209
DavidCrompton1192@gmail.com <DavidCrompton1192@gmail.com>
2022-10-29 11:30:14 -0400

    Auto-import all `.js` files in src/commands

--- deps.js
+++ deps.js
@@ -2,3 +2,4 @@ export * from "https://deno.land/x/discordeno@17.0.0/mod.ts";
 export { v5 } from "https://deno.land/std@0.161.0/uuid/mod.ts";
 export * as log from "https://deno.land/std@0.161.0/log/mod.ts";
 export { DB } from "https://deno.land/x/sqlite@v3.5.0/mod.ts";
+export * from "https://deno.land/std@0.161.0/fs/mod.ts";
--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 // This file responsible for coordinating all other files and actually starting the bot
-import { startBot } from "./deps.js";
+import { startBot, walkSync } from "./deps.js";
 import { bot } from "./bot.js"; // bot instance
 import { updateBotCommands } from "./src/lib/commands.js";
 
-// Commands
-import "./src/commands/quote-commands.js";
-import "./src/commands/roll.js";
-import "./src/commands/ping.js";
-import "./src/commands/moderation/kick.js"
+// Import all commands in commands directory
+for (const file of walkSync("./src/commands")) {
+    if (file.isFile)
+	if (file.name.endsWith(".js"))
+	    import("./"+file.path);
+}
 
 updateBotCommands(bot);
 
