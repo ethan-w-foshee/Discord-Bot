16f77738d8dae2eaa1552135c149df95090b8c6e
Ben <benrankin4000@gmail.com>
2022-11-08 21:57:30 -0500

    Much improved ackInteraction. It now supports effectively all interaction response types and data, including flags

--- src/util/ackInteraction.js
+++ src/util/ackInteraction.js
@@ -1,11 +1,44 @@
 import {
     sendInteractionResponse,
-    InteractionResponseTypes
+    InteractionResponseTypes,
+    ApplicationCommandFlags
 } from "../../deps.js";
 import { bot } from "../../bot.js";
 
-export default function ackInteraction(interaction) {
-  sendInteractionResponse(bot, interaction.id, interaction.token, {
-    type: InteractionResponseTypes.DeferredChannelMessageWithSource,
-  });
+/* Translate simple names into cumbersome discordeno interaction response types */
+const typeList = {
+    thinking: InteractionResponseTypes.DeferredChannelMessageWithSource,
+    deferred: InteractionResponseTypes.DeferredUpdateMessage,
+    message: InteractionResponseTypes.ChannelMessageWithSource,
+    autocomplete: InteractionResponseTypes.ApplicationCommandAutocompleteResult,
+    modal: InteractionResponseTypes.Modal,
+    pong: InteractionResponseTypes.Pong,
+    update: InteractionResponseTypes.UpdateMessage
+};
+
+export default function ackInteraction(interaction, type, flags, data) {
+    /* Default type is thinking (deferred channel message w source) */
+    if ( !(type in typeList) )
+	type = "thinking";
+
+    if ( data == undefined )
+	data = {};
+
+    /* Build based on provided type (translation) and data */
+    const interactionResponse = {
+	type: typeList[type],
+	data: data
+    };
+
+    /* Flags */
+    if ( flags != undefined ) {
+	if (Object.keys(flags).includes("ephemeral") && flags.ephemeral)
+	    interactionResponse['data']['flags'] |= ApplicationCommandFlags.Ephemeral;
+
+	if (Object.keys(flags).includes("suppress_embeds") && flags.suppress_embeds)
+	    interactionResponse['data']['flags'] |= ApplicationCommandFlags.SuppressEmbeds;
+    }
+
+    /* Send response */
+    sendInteractionResponse(bot, interaction.id, interaction.token, interactionResponse);
 }
