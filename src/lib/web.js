1b1ac96fe899c5099c009a82a0a4f052d06d6713
DavidCrompton1192@gmail.com <DavidCrompton1192@gmail.com>
2023-01-07 14:30:50 -0500

    Bug? Discord doesn't support '.' in URLs

--- src/lib/web.js
+++ src/lib/web.js
@@ -3,11 +3,11 @@ import {
 } from "../../deps.js";
 
 function b64e(str) {
-    return b64.encode(str).replace(/\+/g,'_').replace(/=/g,'.');
+    return b64.encode(str).replace(/\+/g,'_').replace(/=/g,'-');
 }
 
 function b64d(str) {
-    return b64.decode(str.replace(/\./g,'=').replace(/_/g,'+'));
+    return b64.decode(str.replace(/-/g,'=').replace(/_/g,'+'));
 }
 
 export function decode(str) {
