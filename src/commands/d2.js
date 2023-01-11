d2335ac803071efb7f92e7a54b50ccab61cc41d4
DavidCrompton1192@gmail.com <DavidCrompton1192@gmail.com>
2023-01-11 18:59:27 -0500

    Fix: D2 Fix (revert)

--- src/commands/d2.js
+++ src/commands/d2.js
@@ -13,18 +13,11 @@ function diagram(bot, msg) {
     const codeRegex = /(?:```d2\n)([\s\S]*\n?)(?:```)/;
     const code = cont.match(codeRegex);
     if (code != null) {
-	const dec = new TextDecoder();
 	const url = `https://starbot.syzygial.cc/d2?code=${encode(code[1])}`;
-	const header = dec.decode(url.slice(0,3));
-	if (header == "PNG") {
-	    sendMessage(bot, msg.channelId, {
-		content: url,
-	    });
-	}else {
-	    sendMessage(bot, msg.channelId, {
-		content: "Bad D2 Program",
-	    });	    
-	}
+
+	sendMessage(bot, msg.channelId, {
+	    content: url,
+	});
     }
 }
 
