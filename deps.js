6796988552c04e0d5d08d8f7400e1fd3e4ef5e10
DavidCrompton1192@gmail.com <DavidCrompton1192@gmail.com>
2023-01-12 18:47:05 -0500

    Can't use templates, unfortunate

--- deps.js
+++ deps.js
@@ -1,14 +1,12 @@
-const stdver = "0.171.0";
+export * from "https://deno.land/x/discordeno@17.1.0/mod.ts";
+export { v5 } from "https://deno.land/std@0.171.0/uuid/mod.ts";
+export * as log from "https://deno.land/std@0.171.0/log/mod.ts";
+export { DB } from "https://deno.land/x/sqlite@v3.7.0/mod.ts";
+export * from "https://deno.land/std@0.171.0/fs/mod.ts";
+export { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+export * from "https://deno.land/x/imagescript@v1.2.15/mod.ts";
 
-export * from `https://deno.land/x/discordeno@17.1.0/mod.ts`;
-export { v5 } from `https://deno.land/std@${stdver}/uuid/mod.ts`;
-export * as log from `https://deno.land/std@${stdver}/log/mod.ts`;
-export { DB } from `https://deno.land/x/sqlite@v3.7.0/mod.ts`;
-export * from `https://deno.land/std@${stdver}/fs/mod.ts`;
-export { Application, Router } from `https://deno.land/x/oak@v11.1.0/mod.ts`;
-export * from `https://deno.land/x/imagescript@v1.2.15/mod.ts`;
+export * as b64 from "https://deno.land/std@0.171.0/encoding/base64.ts";
+export * from "https://deno.land/x/compress@v0.4.5/mod.ts";
 
-export * as b64 from `https://deno.land/std@${stdver}/encoding/base64.ts`;
-export * from `https://deno.land/x/compress@v0.4.5/mod.ts`;
-
-export * from `https://deno.land/std@${stdver}/streams/conversion.ts`;
+export * from "https://deno.land/std@0.171.0/streams/conversion.ts";
