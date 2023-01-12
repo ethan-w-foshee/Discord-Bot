7d8dc78005138903183d8375692c94ace03c0a3d
DavidCrompton1192@gmail.com <DavidCrompton1192@gmail.com>
2023-01-12 18:58:48 -0500

    Missing a v

--- deps.js
+++ deps.js
@@ -4,7 +4,7 @@ export * as log from "https://deno.land/std@0.171.0/log/mod.ts";
 export { DB } from "https://deno.land/x/sqlite@v3.7.0/mod.ts";
 export * from "https://deno.land/std@0.171.0/fs/mod.ts";
 export { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
-export * from "https://deno.land/x/imagescript@v1.2.15/mod.ts";
+export * from "https://deno.land/x/imagescript@1.2.15/mod.ts";
 
 export * as b64 from "https://deno.land/std@0.171.0/encoding/base64.ts";
 export * from "https://deno.land/x/compress@v0.4.5/mod.ts";
