a265736c78505fec28560aa9e6e46c9ca44c5ef3
DavidCrompton1192@gmail.com <DavidCrompton1192@gmail.com>
2023-01-12 18:32:18 -0500

    Update Deps

--- deps.js
+++ deps.js
@@ -1,12 +1,14 @@
-export * from "https://deno.land/x/discordeno@17.0.0/mod.ts";
-export { v5 } from "https://deno.land/std@0.161.0/uuid/mod.ts";
-export * as log from "https://deno.land/std@0.161.0/log/mod.ts";
-export { DB } from "https://deno.land/x/sqlite@v3.5.0/mod.ts";
-export * from "https://deno.land/std@0.161.0/fs/mod.ts";
-export { Application, Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
-export * from "https://deno.land/x/imagescript@v1.2.14/mod.ts";
+const stdver = "0.171.0";
 
-export * as b64 from "https://deno.land/std@0.163.0/encoding/base64.ts";
-export * from "https://deno.land/x/compress@v0.4.5/mod.ts";
+export * from `https://deno.land/x/discordeno@17.1.0/mod.ts`;
+export { v5 } from `https://deno.land/std@${stdver}/uuid/mod.ts`;
+export * as log from `https://deno.land/std@${stdver}/log/mod.ts`;
+export { DB } from `https://deno.land/x/sqlite@v3.7.0/mod.ts`;
+export * from `https://deno.land/std@${stdver}/fs/mod.ts`;
+export { Application, Router } from `https://deno.land/x/oak@v11.1.0/mod.ts`;
+export * from `https://deno.land/x/imagescript@v1.2.15/mod.ts`;
 
-export * from "https://deno.land/std@0.171.0/streams/conversion.ts";
+export * as b64 from `https://deno.land/std@${stdver}/encoding/base64.ts`;
+export * from `https://deno.land/x/compress@v0.4.5/mod.ts`;
+
+export * from `https://deno.land/std@${stdver}/streams/conversion.ts`;
