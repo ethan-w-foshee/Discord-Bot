4b78b5e503d71df950b02e060a59793007cd6080
ethan.w.foshee <skyhailer2@gmail.com>
2023-01-17 22:20:37 -0600

    Testing/screwing with discord message commands

--- src/commands/stupidenglish.js
+++ src/commands/stupidenglish.js
@@ -35,13 +35,16 @@ addBotCommand(bot, {
 	actions: [
 		function (_bot, interaction) {
 			/* Get options */
+			const messageObject =
+        interaction.data.resolved.messages.values().next().value;
+
 			const options = interaction.data.options
 
-			const inval = options.filter((option) =>
+			messageObject = options.filter((option) =>
 				option.name == "content"
 			)[0].value;
 
-			ackInteraction(interaction, "message", '', { content: stupidify(inval) })
+			ackInteraction(interaction, "message", '', { content: stupidify(messageObject) })
 		},
 	],
 });
