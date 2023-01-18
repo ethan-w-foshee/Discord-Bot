f9ae819cf9c162bbd909aff27852a4f80dfadbcd
ethan.w.foshee <skyhailer2@gmail.com>
2023-01-17 22:03:57 -0600

    Testing app stupid english

--- src/commands/stupidenglish.js
+++ src/commands/stupidenglish.js
@@ -1,29 +1,47 @@
-import { ApplicationCommandOptionTypes} from "../../deps.js";
+import { ApplicationCommandOptionTypes } from "../../deps.js";
 import { bot } from "../../bot.js";
 import { addBotCommand } from "../lib/commands.js";
 import { stupidify } from "../util/stupidenglish.js";
 import ackInteraction from "../util/ackInteraction.js";
 
 addBotCommand(bot, {
-    description: "Send a message but without a few letters",
-    name: "stupidenglish",
-    options: [{
-	name: "content",
-	description: "The original message to be \"corrected\"",
-	type: ApplicationCommandOptionTypes.String,
-	required: true,
-    }],
-    type: "slash",
-    actions: [
-	function (_bot, interaction) {
-	    /* Get options */
-	    const options = interaction.data.options
+	description: "Send a message but without a few letters",
+	name: "stupidenglish",
+	options: [{
+		name: "content",
+		description: "The original message to be \"corrected\"",
+		type: ApplicationCommandOptionTypes.String,
+		required: true,
+	}],
+	type: "slash",
+	actions: [
+		function (_bot, interaction) {
+			/* Get options */
+			const options = interaction.data.options
 
-	    const inval = options.filter((option) =>
-		option.name == "content"
-	    )[0].value;
+			const inval = options.filter((option) =>
+				option.name == "content"
+			)[0].value;
 
-	    ackInteraction(interaction, "message", '' , {content: stupidify(inval)})
-	}
-    ]
+			ackInteraction(interaction, "message", '', { content: stupidify(inval) })
+		}
+	]
+});
+
+addBotCommand(bot, {
+	description: "Stupify a message",
+	name: "stupidenglish",
+	type: "message",
+	actions: [
+		function (bot, interaction) {
+			/* Get options */
+			const options = interaction.data.options
+
+			const inval = options.filter((option) =>
+				option.name == "content"
+			)[0].value;
+
+			ackInteraction(interaction, "message", '', { content: stupidify(inval) })
+		},
+	],
 });
