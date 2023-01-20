fc996c1ccbc4a4867ec1b561547061f4ce10d9b4
DavidCrompton1192@gmail.com <DavidCrompton1192@gmail.com>
2023-01-19 21:42:44 -0500

    D2 Features

--- src/lib/d2.js
+++ src/lib/d2.js
@@ -1,8 +1,47 @@
-export async function renderD2(code) {
+const themes = [
+    "0",
+    "1",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "100",
+    "101",
+    "102",
+    "103",
+    "104",
+    "105"
+];
+
+const layouts = [
+    "dagre",
+    "elk"
+];
+
+export async function renderD2(code, opts) {
     const enc = new TextEncoder();
 
+    const theme = opts?.theme;
+
+    const sketch = opts?.sketch;
+
+    const layout = opts?.layout;
+
+    const cmd = ["d2"];
+
+    if (sketch)
+	cmd.push("-s");
+    if (theme in themes)
+	cmd.push(...["-t",theme]);
+    if (layout in layouts)
+	cmd.push(...["-l",layout]);
+
+    cmd.push(...["-","-"])
+    
     const d2 = Deno.run({
-	cmd: ["d2","-","-"],
+	cmd,
 	stderr: "piped",
 	stdout: "piped",
 	stdin: "piped"
@@ -21,6 +60,7 @@ export async function renderD2(code) {
     await d2.stdin.write(enc.encode(code));
     await d2.stdin.close();
     const svg = await d2.output();
+    await d2.stderr.close()
     await d2.close();
 
     let i = 0;
@@ -31,6 +71,7 @@ export async function renderD2(code) {
     } while(n)
     await rsvg.stdin.close();
     const png = await rsvg.output();
+    await rsvg.stderr.close()
     await rsvg.close();
     
     return png;
