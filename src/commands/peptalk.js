70767bc062a77d4398c02b2cac3b4c75714f3a8f
Ben <benrankin4000@gmail.com>
2023-04-07 20:56:38 -0400

    Add probably functioning peptalk command

---
+++ src/commands/peptalk.js
@@ -0,0 +1,21 @@
+import {
+    ApplicationCommandOptionTypes,
+    editOriginalInteractionResponse,
+} from "../../deps.js";
+import { bot } from "../../bot.js";
+import { addBotcommand } from "../lib/commands.js";
+import { genPeptalk } from "../util/peptalk/genPeptalk.js";
+import ackInteraction from "../util/ackInteraction.js";
+
+addBotCommand(bot, {
+    description: "Pump yourself up with a peptalk",
+    name: "peptalk",
+    type: "slash",
+    actions: [
+	function (bot, interaction) {
+	    // Don't bother "thinking" because if it can't generate quickly enough for the timeout,
+	    // there are other problems so whatever
+	    ackInteraction(interaction, "message", {}, await genPeptalk());
+	}
+    ]
+});
---
+++ src/util/peptalk/genPeptalk.js
@@ -0,0 +1,41 @@
+// Generate a pep talk
+
+import { logger } from "../../../logger.js";
+
+export async function genPeptalk() {
+    // Get all the peptalk options
+    const options = await loadOptions()
+
+    let pepTalk = ""
+
+    // For each phase (there are 4), choose a random one from
+    // the list of options and append it to the pepTalk string
+    for (let i = 0; i < 4; i++) {
+	const choice = Math.floor(Math.random() * 100) % options[i].length
+	pepTalk += options[i][choice]
+    }
+
+    return pepTalk
+}
+
+async function loadOptions() {
+    const ret = []
+
+    // Define all the input files to read
+    const inputFiles = ["phase1.txt", "phase2.txt", "phase3.txt", "phase4.txt"]
+
+    for (const file of inputFiles) {
+
+	// Read then split by lines
+	const raw = await Deno.readTextFile(file)
+	let lines = raw.split("\n")
+
+	// Sanitization. Ignore comments (#) or empty lines
+	lines = lines.filter(line => (line.length != 0 && line[0] != "#"))
+
+	// Add to "ret"
+	ret.push(lines)
+    }
+
+    return ret
+}
