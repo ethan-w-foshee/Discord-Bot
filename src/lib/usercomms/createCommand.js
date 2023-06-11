2eb18d7714dbe9711185f72d9c5da10dcec4eeb3
DavidCrompton1192@gmail.com <DavidCrompton1192@gmail.com>
2023-06-11 16:55:52 -0400

    Basic Create Command "Interaction"

--- src/lib/usercomms/createCommand.js
+++ src/lib/usercomms/createCommand.js
@@ -1,3 +1,21 @@
-export function createCommand(_bot, _interaction) {
+import { ackInteraction } from "../../util/ackInteraction.js";
+import {
+    editOriginalInteractionResponse,
+} from "../../../deps.js";
+
+export function createCommand(bot, interaction) {
+    ackInteraction(
+	interaction,
+	"thinking",
+	{ephemeral: true},
+	{content: `Creating command now...`}
+    );
+    editOriginalInteractionResponse(
+	bot,
+	interaction.token,
+	{
+	    content: "Command created!"
+	}
+    );
     return
 }
