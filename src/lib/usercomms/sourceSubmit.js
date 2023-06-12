89d5401e99efca7b3203dd5e1a7d31af56374e03
DavidCrompton1192@gmail.com <DavidCrompton1192@gmail.com>
2023-06-12 19:03:52 -0400

    BigInt BadInt

--- src/lib/usercomms/sourceSubmit.js
+++ src/lib/usercomms/sourceSubmit.js
@@ -2,9 +2,7 @@ import ackInteraction from "../../util/ackInteraction.js";
 import {
 } from "../../../deps.js";
 
-
 export function sourceCommand(bot, interaction) {
-    bot.logger.debug(`${JSON.stringify(interaction)}`);
     ackInteraction(interaction, "message", {ephemeral: true}, {
 	content: "Updated command."
     });
