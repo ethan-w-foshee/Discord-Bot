5f7f0c4ae3999b7c3c33efa7cbd33d7d1093a4d9
DavidCrompton1192@gmail.com <DavidCrompton1192@gmail.com>
2023-07-20 18:24:25 -0400

    Fix Logging in Command Creation and Submission

--- src/lib/usercomms/sourceSubmit.js
+++ src/lib/usercomms/sourceSubmit.js
@@ -6,7 +6,12 @@ export function sourceCommand(bot, interaction) {
     ackInteraction(interaction, "message", {ephemeral: true}, {
 	content: "Updated command."
     });
+    const sourceData = interaction.data.options.filter(
+	o => o.name.endsWith("_src_", o.name.length-"create".length)
+    )[0];
+    const sourceName = sourceData.name;
+
     // Using Command Name, create/update the command in the DB
-    bot.logger.debug(`Creating command from... ${JSON.stringify(interaction)}`);
+    bot.logger.debug(`Creating command for : ${sourceName}`);
     return
 }
