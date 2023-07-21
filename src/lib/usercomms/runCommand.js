630c37a232e4d1355fc78edb4eaf82b56d41ab4e
DavidCrompton1192@gmail.com <DavidCrompton1192@gmail.com>
2023-07-21 00:10:18 -0400

    Add Run and Delete Command

---
+++ src/lib/usercomms/runCommand.js
@@ -0,0 +1,40 @@
+import ackInteraction from "../../util/ackInteraction.js";
+import {
+} from "../../../deps.js";
+import { usergameDB } from "./commandDB.js";
+
+export async function runCommand(bot, interaction) {
+    const createOptions = interaction.data.options.filter(
+	o => o.name == "run"
+    )[0].options;
+
+    const commandId = createOptions.filter(
+	o => o.name == "command"
+    )[0]["value"];
+
+    bot.logger.debug(`Running command ${commandId}`);
+
+    const exists = usergameDB.searchCommand({name: commandId})
+
+    if (exists.length == 0) {
+	ackInteraction(
+	    interaction,
+	    "message",
+	    {ephemeral: true},
+	    {
+		content: `Command with name ${commandId} does not exist!`
+	    }
+	);
+    }else {
+	ackInteraction(
+	    interaction,
+	    "thinking",
+	    {ephemeral: true}
+	);
+	const output = await usergameDB.runCommand(commandId);
+	editOriginalInteractionResponse(bot, interaction.token, {
+	    content: output.slice(0,2000)
+	});
+    }
+    return
+}
