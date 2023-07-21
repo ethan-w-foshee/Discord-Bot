3b9b87fcc20fcf4fe093b991702d9a98dbf19601
DavidCrompton1192@gmail.com <DavidCrompton1192@gmail.com>
2023-07-20 21:12:37 -0400

    Implement Source Update & Creation

--- src/lib/usercomms/sourceSubmit.js
+++ src/lib/usercomms/sourceSubmit.js
@@ -1,22 +1,46 @@
 import ackInteraction from "../../util/ackInteraction.js";
 import {
 } from "../../../deps.js";
+import { usergameDB } from "./commandDB.js";
 
 export function sourceCommand(_bot, interaction) {
     const sourceData = interaction.data;
     const sourceId = sourceData.customId;
     const commandName = sourceId.slice(9,-11)
     const commandSource = sourceData.components[0].components[0].value;
+    const userId = sourceData.member.id;
 
     if (sourceId.endsWith("create")) {
-	console.log("TODO");
+	const success = usergameDB.createCommand(
+	    `${userId}`,
+	    commandName,
+	    commandSource
+	);
+	if (success) {
+	    ackInteraction(interaction, "message", {ephemeral: true}, {
+		content: `Created Command "${commandName}":\`\`\`${commandSource.slice(0,1500)}\`\`\``
+	    });
+	}else {
+	    ackInteraction(interaction, "message", {ephemeral: true}, {
+		content: `Failed to Create Command ${commandName}`
+	    });
+	}
     }else if (sourceId.endsWith("update")) {
-	console.log("TODO");
+	const success = usergameDB.updateCommand(
+	    `${userId}`,
+	    commandName,
+	    commandSource
+	);
+	if (success) {
+	    ackInteraction(interaction, "message", {ephemeral: true}, {
+		content: `Updated Command "${commandName}" to:\`\`\`${commandSource.slice(0,1500)}\`\`\``
+	    });
+	}else {
+	    ackInteraction(interaction, "message", {ephemeral: true}, {
+		content: `Failed to Create Command ${commandName}`
+	    });
+	}
     }
-
-    ackInteraction(interaction, "message", {ephemeral: true}, {
-	content: `Created Command "${commandName}":\`\`\`${commandSource.slice(0,1500)}\`\`\``
-    });
 
     return
 }
