29a76e72cc2927d8fda417d93e9c7020731622c8
DavidCrompton1192@gmail.com <DavidCrompton1192@gmail.com>
2023-07-20 20:12:03 -0400

    Me no Thinky

--- src/lib/usercomms/sourceSubmit.js
+++ src/lib/usercomms/sourceSubmit.js
@@ -3,9 +3,7 @@ import {
 } from "../../../deps.js";
 
 export function sourceCommand(_bot, interaction) {
-    const sourceData = interaction.data.options.filter(
-	o => o.customId.endsWith("src",o.customId.length-"_create".length)
-    )[0];
+    const sourceData = interaction.data;
     const sourceId = sourceData.customId;
     const commandName = sourceId.slice(9,-11)
     const commandSource = sourceData.components[0].components.value;
