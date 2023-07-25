e165dcbad38742e3576e4af305420865ba3d3aac
DavidCrompton1192@gmail.com <DavidCrompton1192@gmail.com>
2023-07-24 21:10:41 -0400

    Add Message Command for Running UserCommands

--- src/lib/usercomms/masterAction.js
+++ src/lib/usercomms/masterAction.js
@@ -1,6 +1,9 @@
 import {
     InteractionTypes,
+    MessageComponentTypes,
+    TextStyles,
 } from "../../../deps.js";
+import ackInteraction from "../../util/ackInteraction.js";
 
 import { createCommand } from "../../lib/usercomms/createCommand.js";
 import { updateCommand } from "../../lib/usercomms/updateCommand.js";
@@ -50,4 +53,44 @@ export function masterAction(bot, interaction) {
 	    return;
 	}
     }
+}
+
+export function masterRunAction(bot, interaction) {
+    switch(interaction.type) {
+	case InteractionTypes.ModalSubmit:
+	case InteractionTypes.MessageComponent: {
+	    if (!data.customId.includes("userrun"))
+		return;
+	    const commInstr = data.customId.split('_');
+	    const action = commInstr[1];
+	    if (action in subcommands) {
+		subcommands[action](bot, interaction)
+		return;
+	    }
+	    break;
+	}
+	case InteractionTypes.ApplicationCommand: {
+	    ackInteraction(
+		interaction,
+		"modal",
+		{},
+		{
+		    customId: `userrun_run`,
+		    title: `Run a Command`,
+		    components: [{
+			type: MessageComponentTypes.ActionRow,
+			components: [{
+			    type: MessageComponentTypes.InputText,
+			    customId: `command`,
+			    style: TextStyles.Short,
+			    label: "Command Name"
+			}]	
+		    }]
+		}
+	    );
+	    break;
+	} default: {
+	    return;
+	}
+    }    
 }
