9a63d5d76a9c930e6459e4179166bf1b0208b970
david <davidcrompton@duck.com>
2024-04-01 18:34:25 -0400

    Ping: report time to respond

--- src/commands/ping.c
+++ src/commands/ping.c
@@ -3,20 +3,23 @@
 #include <time.h>
 
 void command_pong(struct discord *client, const struct discord_interaction *event) {
-  u64snowflake time_sent = event->id / 4194304 + 1420070400000;
+  u64snowflake time_sent = (event->id >> 22) + 1420070400000;
   struct timespec now;
   clock_gettime(CLOCK_REALTIME, &now);
 
-  u64snowflake diff = time_sent - (now.tv_sec * 1000 + now.tv_nsec / 1000);
+  // This time sometimes is negative? idk if that's because of a
+  // problem with my conversion (probably) or with time syncronization
+  // between Discord's server etc.
+  double diff = (now.tv_sec * 1000 + (double) now.tv_nsec / 1000000) - time_sent;
 
   char response[512];
 
-  sprintf(&response[0], "Pong! %llu ms", diff);
+  snprintf(&response[0], sizeof(response), "Pong! %.3g ms", diff);
   
   struct discord_interaction_response params = {
     .type = DISCORD_INTERACTION_CHANNEL_MESSAGE_WITH_SOURCE,
     .data = &(struct discord_interaction_callback_data){
-      .content = "pong"
+      .content = &response[0]
     }
   };
   discord_create_interaction_response(client, event->id,
